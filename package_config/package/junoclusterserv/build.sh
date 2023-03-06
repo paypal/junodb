7f66e80fb787eb577957e10f95e8040fa6771fa8
Nitish Tripathi <nittripathi@paypal.com>
2023-03-06 14:32:39 -0700

    Removing staggering files remaining

--- package_config/package/junoclusterserv/build.sh
+++ package_config/package/junoclusterserv/build.sh
@@ -16,8 +16,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #  
-#  Package utility provides the utility interfaces for mux package
-#  
+
 #! /bin/bash
 
 pkg_dir=${1:-$BUILDTOP/package_config/package/junoclusterserv}
